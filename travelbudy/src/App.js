ebd76f2ff643e132a35ea3eb2d3f7e770eff8d78
weerawi <weerawikramatharindu@gmail.com>
2023-04-24 22:38:34 +0530

    start to create Navbar -icon ,image examples ,fonts and colors are added - they call in one index.js

--- travelbudy/src/App.js
+++ travelbudy/src/App.js
@@ -1,5 +1,11 @@
+import Main from "./pages/Main/Main";
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
 }
 
 export default App;
