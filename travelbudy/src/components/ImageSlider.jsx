f2622765ee651ab47c4def1d6e5957b54101e3b0
weerawi <weerawikramatharindu@gmail.com>
2023-04-27 08:38:47 +0530

    slider title and searchtips button created

--- travelbudy/src/components/ImageSlider.jsx
+++ travelbudy/src/components/ImageSlider.jsx
@@ -1,15 +1,9 @@
 import React, { useState } from "react";
-import "./ImageSlider.css";
 import { icons } from "../constants";
+import "./ImageSliderStyle.css";
 
 const ImageSlider = ({ slides }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-
-  const sliderStyles = {
-    height: "100%",
-    position: "relative",
-    margin: "0",
-  };
 
   const slideStyles = {
     backgroundImage: `url(${slides[currentIndex].url})`,
@@ -17,32 +11,7 @@ const ImageSlider = ({ slides }) => {
     backgroundSize: "cover",
     backgroundAttachment: "fixed",
     backgroundPosition: "center ",
-    height: "90vh",
-  };
-
-  const leftArrow = {
-    position: "absolute",
-    top: "50%",
-    transform: "translate(0,-50%)",
-    left: "32px",
-    width: "45px",
-    height: "45px",
-    zIndex: 1,
-    cursor: "pointer",
-    border: "2px solid rgb(211, 211, 211)",
-    borderRadius: "50%",
-  };
-  const rightArrow = {
-    position: "absolute",
-    top: "50%",
-    transform: "translate(0,-50%)",
-    right: "32px",
-    width: "45px",
-    height: "45px",
-    zIndex: 1,
-    cursor: "pointer",
-    border: "2px solid black",
-    borderRadius: "50%",
+    height: "80vh",
   };
 
   const goPrevious = () => {
@@ -59,21 +28,45 @@ const ImageSlider = ({ slides }) => {
     setCurrentIndex(newIndex);
   };
 
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
   return (
-    <div style={sliderStyles}>
+    <div className="sliderStyles">
       <img
+        className="arrow leftArrow"
         src={icons.chevronLeft}
         alt="left"
-        style={leftArrow}
         onClick={goPrevious}
       ></img>
       <img
+        className="arrow rightArrow"
         src={icons.chevronRight}
         alt="right"
-        style={rightArrow}
         onClick={goNext}
       ></img>
-      <div style={slideStyles}>hi</div>
+      {/* 
+      /////////////// slider text and others type on this div /////////////  */}
+      <div style={slideStyles}>
+        <div className="title">Dreamy summer holidays </div>
+
+        <div className="searchTips">search tips</div>
+
+        <div className="sliderSearchBar">search bars </div>
+      </div>
+
+      <div className="dotsContainer">
+        {slides.map((slide, slideIndex) => (
+          <div
+            className="dot"
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+          >
+            ⊙{" "}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
