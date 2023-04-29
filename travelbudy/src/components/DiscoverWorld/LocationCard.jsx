9534b04295b93060056f844f398fd7a29c364855
weerawi <weerawikramatharindu@gmail.com>
2023-04-29 18:17:20 +0530

    using function to hover styling change - in LocationCard

--- travelbudy/src/components/DiscoverWorld/LocationCard.jsx
+++ travelbudy/src/components/DiscoverWorld/LocationCard.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-const LocationCard = (props) => {
-  const LocationCardStyles = {
-    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.3)) ,url(${props.image})`,
-    backgroundRepeat: " no-repeat",
-    backgroundSize: "cover",
-    backgroundPosition: "center ",
-    height: "200px",
+const useFade = (props) => {
+  const [hover, setHover] = useState(false);
+
+  const onMouseEnter = () => {
+    setHover(true);
   };
 
+  const onMouseLeave = () => {
+    setHover(false);
+  };
+
+  const hoverStyle = !hover
+    ? {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.3)) ,url(${props.image})`,
+        backgroundRepeat: " no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center ",
+        height: "200px",
+      }
+    : {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.1)),url(${props.image}) `,
+        backgroundRepeat: " no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center ",
+        height: "200px",
+      };
+
+  return { hoverStyle, onMouseEnter, onMouseLeave };
+};
+
+const LocationCard = (props, { styles }) => {
+  const { hoverStyle, ...fadeProps } = useFade(props);
+
   return (
-    <a style={LocationCardStyles} className="location-card">
+    <a style={{ ...hoverStyle }} {...fadeProps} className="location-card">
       <div className="location-name">{props.name}</div>
     </a>
   );
